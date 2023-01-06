e44f3f16ad77a032696506080ca42771ff6854d5
LYErikuy <111875719+Buelie@users.noreply.github.com>
2023-01-06 17:37:08 +0800

    Create Main.js

---
+++ src/api/Main.js
@@ -0,0 +1,15 @@
+export function col_info(info){
+	try{
+		var date = new Date().toLocaleString();
+		console.log("["+date+" | INFO]:"+date);
+		return 0;
+	}
+	catch(eor){
+		var date = new Date().toLocaleString();
+		console.log("["+date+" | ERROR]:"+eor);
+		return 100;
+	}
+	finally{
+		return 900;
+	}
+}
