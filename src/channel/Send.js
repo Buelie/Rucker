aeff29b39afe73a762323623667cd31d6181d09e
LYErikuy <111875719+Buelie@users.noreply.github.com>
2023-01-09 18:06:39 +0800

    Error

--- src/channel/Send.js
+++ src/channel/Send.js
@@ -2,5 +2,9 @@ xmlhttp.open("POST","https://www.kookapp.cn/api/v3",true);
 xmlhttp.send();
 
 function send(info){
-	
+    var ero = {
+        "code" : 0, // integer, 错误码，0代表成功，非0代表失败，具体的错误码参见错误码一览
+        "message" : "error info", // string, 错误消息，具体的返回消息会根据Accept-Language来返回。
+        "data" : [], // mixed, 具体的数据。
+    }
 }
