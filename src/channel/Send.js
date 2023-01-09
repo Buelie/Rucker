cb35417bd8c3bd9b3993540689c80618e87d8857
LYErikuy <111875719+Buelie@users.noreply.github.com>
2023-01-09 20:54:44 +0800

    Update Send.js

--- src/channel/Send.js
+++ src/channel/Send.js
@@ -1,10 +1,8 @@
-xmlhttp.open("POST","https://www.kookapp.cn/api/v3/message/createapi/v3",true);
-xmlhttp.send();
-
 function send(info){
-    var ero = {
-        "code" : 0, // integer, 错误码，0代表成功，非0代表失败，具体的错误码参见错误码一览
-        "message" : "error info", // string, 错误消息，具体的返回消息会根据Accept-Language来返回。
-        "data" : [], // mixed, 具体的数据。
+    var send = {
+        "type": 1,
+        "content": info
     }
+    xmlhttp.open("POST","https://www.kookapp.cn/api/v3/message/createapi/v3",true);
+    xmlhttp.send(send);
 }
