39abc863809dda3012f904cabd065f07743b8973
LYErikuy <111875719+Buelie@users.noreply.github.com>
2023-01-09 13:12:18 +0800

    Update Main.js

--- src/api/Main.js
+++ src/api/Main.js
@@ -1,16 +1,19 @@
 import './api'
+
+xmlhttp.open("GET","https://www.kookapp.cn/api/v3",true);
+xmlhttp.send();
 export function col_info(info){
-	try{
-		var date = new Date().toLocaleString();
-		console.log("["+date+" | INFO]:"+date);
-		return 0;
-	}
-	catch(eor){
-		var date = new Date().toLocaleString();
-		console.log("["+date+" | ERROR]:"+eor);
-		return 100;
-	}
-	finally{
-		return 900;
-	}
+    try{
+	var date = new Date().toLocaleString();
+	console.log("["+date+" | INFO]:"+date);
+	return 0;
+    }
+    catch(eor){
+	var date = new Date().toLocaleString();
+	console.log("["+date+" | ERROR]:"+eor);
+	return 100;
+    }
+    finally{
+	return 900;
+    }
 }
