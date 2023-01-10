7eaa858bbe4f0d21af01024f10616ed247cf6156
LYErikuy <111875719+Buelie@users.noreply.github.com>
2023-01-10 11:29:05 +0800

    Update View.js

--- src/user/View.js
+++ src/user/View.js
@@ -1,4 +1,48 @@
-function view(){
+function view(mode){
   xmlhttp.open("POST","https://www.kookapp.cn/api/v3/user/view",true);
   xmlhttp.send();
+  var text = xmlhttp.responseText;
+  if (mode == 'id'){
+    console.log("Data return status:"+text.message+",Return status code:"+text.code);
+    return text.data.id;
+  }else if (mode == 'username'){
+    console.log("Data return status:"+text.message+",Return status code:"+text.code);
+    return text.data.username;
+  }else if (mode == 'nickname'){
+    console.log("Data return status:"+text.message+",Return status code:"+text.code);
+    return text.data.nickname;
+  }else if (mode == 'username'){
+    console.log("Data return status:"+text.message+",Return status code:"+text.code);
+    return text.data.username;
+  }else if (mode == 'username'){
+    console.log("Data return status:"+text.message+",Return status code:"+text.code);
+    return text.data.username;
+  }else if (mode == 'username'){
+    console.log("Data return status:"+text.message+",Return status code:"+text.code);
+    return text.data.username;
+  }else if (mode == 'username'){
+    console.log("Data return status:"+text.message+",Return status code:"+text.code);
+    return text.data.username;
+  }else if (mode == 'username'){
+    console.log("Data return status:"+text.message+",Return status code:"+text.code);
+    return text.data.username;
+  }else if (mode == 'username'){
+    console.log("Data return status:"+text.message+",Return status code:"+text.code);
+    return text.data.username;
+  }else if (mode == 'username'){
+    console.log("Data return status:"+text.message+",Return status code:"+text.code);
+    return text.data.username;
+  }else if (mode == 'username'){
+    console.log("Data return status:"+text.message+",Return status code:"+text.code);
+    return text.data.username;
+  }else if (mode == 'username'){
+    console.log("Data return status:"+text.message+",Return status code:"+text.code);
+    return text.data.username;
+  }else if (mode == 'username'){
+    console.log("Data return status:"+text.message+",Return status code:"+text.code);
+    return text.data.username;
+  }else if (mode == 'username'){
+    console.log("Data return status:"+text.message+",Return status code:"+text.code);
+    return text.data.username;
+  }
 }
