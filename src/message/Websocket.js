2e39406c6dcb9602267eabe1fac83c65c80636a3
LYErikuy <111875719+Buelie@users.noreply.github.com>
2023-01-10 08:49:30 +0800

    Establish a websocket link
    建立websocket链接

--- src/message/Websocket.js
+++ src/message/Websocket.js
@@ -1,2 +1,12 @@
-xmlhttp.open("GET","https://www.kookapp.cn/api/v3",true);
-xmlhttp.send();
+var json = {
+	"compress":"0"
+}
+xmlhttp.open("GET","https://www.kookapp.cn/api/v3/gateway/index",true);
+xmlhttp.send(json);
+var url = xmlhttp.responseText; 
+
+setTimeout(function () {
+	var soc = new WebSocket("https://developer.kookapp.cn/"){
+	    send(url.url);
+	}
+}, 300);
