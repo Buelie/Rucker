60feeff0a77e4e3aadcf4d4f5ae937c4dad22c96
LYErikuy <111875719+Buelie@users.noreply.github.com>
2023-01-10 08:35:10 +0800

    Update Webhook.js

--- src/message/Webhook.js
+++ src/message/Webhook.js
@@ -1,2 +1,6 @@
-xmlhttp.open("GET","https://www.kookapp.cn/api/v3",true);
-xmlhttp.send();
+function webhook(url){
+  sessionStorage.setItem("CallbackUrl", url);
+  var json = sessionStorage.getItem("CallbackUrl");
+  xmlhttp.open("POST","https://www.kookapp.cn/api/v3",true);
+  xmlhttp.send(json);
+}
