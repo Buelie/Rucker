d12e6cab6d553db8d855759d747cb074565d54c2
LYErikuy <111875719+Buelie@users.noreply.github.com>
2023-01-10 11:33:31 +0800

    Update View.js

--- src/user/View.js
+++ src/user/View.js
@@ -11,38 +11,41 @@ function view(mode){
   }else if (mode == 'nickname'){
     console.log("Data return status:"+text.message+",Return status code:"+text.code);
     return text.data.nickname;
-  }else if (mode == 'username'){
+  }else if (mode == 'identify_num'){
     console.log("Data return status:"+text.message+",Return status code:"+text.code);
-    return text.data.username;
-  }else if (mode == 'username'){
+    return text.data.identify_num;
+  }else if (mode == 'online'){
     console.log("Data return status:"+text.message+",Return status code:"+text.code);
-    return text.data.username;
-  }else if (mode == 'username'){
+    return text.data.online;
+  }else if (mode == 'status'){
     console.log("Data return status:"+text.message+",Return status code:"+text.code);
-    return text.data.username;
-  }else if (mode == 'username'){
+    return text.data.status;
+  }else if (mode == 'avatar'){
     console.log("Data return status:"+text.message+",Return status code:"+text.code);
-    return text.data.username;
-  }else if (mode == 'username'){
+    return text.data.avatar;
+  }else if (mode == 'vip_avatar'){
     console.log("Data return status:"+text.message+",Return status code:"+text.code);
-    return text.data.username;
-  }else if (mode == 'username'){
+    return text.data.vip_avatar;
+  }else if (mode == 'is_vip'){
     console.log("Data return status:"+text.message+",Return status code:"+text.code);
-    return text.data.username;
-  }else if (mode == 'username'){
+    return text.data.is_vip;
+  }else if (mode == 'bot'){
     console.log("Data return status:"+text.message+",Return status code:"+text.code);
-    return text.data.username;
-  }else if (mode == 'username'){
+    return text.data.bot;
+  }else if (mode == 'mobile_verified'){
     console.log("Data return status:"+text.message+",Return status code:"+text.code);
-    return text.data.username;
-  }else if (mode == 'username'){
+    return text.data.mobile_verified;
+  }else if (mode == 'roles'){
     console.log("Data return status:"+text.message+",Return status code:"+text.code);
-    return text.data.username;
-  }else if (mode == 'username'){
+    return text.data.roles;
+  }else if (mode == 'joined_at'){
     console.log("Data return status:"+text.message+",Return status code:"+text.code);
-    return text.data.username;
-  }else if (mode == 'username'){
+    return text.data.joined_at;
+  }else if (mode == 'active_time'){
     console.log("Data return status:"+text.message+",Return status code:"+text.code);
-    return text.data.username;
+    return text.data.active_time;
+  }else{
+    console.log("Data return status:"+text.message+",Return status code:"+text.code);
+    return 'error';
   }
 }
