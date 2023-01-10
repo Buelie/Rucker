c36834ee405f8d9b25e2779a024fd5e2d5d51665
LYErikuy <111875719+Buelie@users.noreply.github.com>
2023-01-10 14:43:48 +0800

    Create Bot.js

---
+++ src/Bot/Bot.js
@@ -0,0 +1,9 @@
+import './command'
+
+function (token){
+  xmlhttp.open("","https://www.kookapp.cn/api/v3Authorization: Bot "+token.token);
+  xmlhttp.send();
+  function command(command){
+    return 0;
+  }
+}
