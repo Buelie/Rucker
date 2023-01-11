59fc858b89d6fac65dc810c222519f6b22ff433a
LYErikuy <111875719+Buelie@users.noreply.github.com>
2023-01-11 10:06:10 +0800

    Update Websocket.js

--- src/message/Websocket.js
+++ src/message/Websocket.js
@@ -3,12 +3,10 @@ var json = {
 }
 xmlhttp.open("GET","https://www.kookapp.cn/api/v3/gateway/index",true);
 xmlhttp.send(json);
-var url = xmlhttp.responseText; 
-var pong = "";
-var hello = "";
+var url = xmlhttp.responseText;
 
 setTimeout(function () {
-	var soc = new WebSocket(url.url){
+	var soc = new WebSocket(url.data.url){
 	    send(pong);
 	    send(hello);
 	}
