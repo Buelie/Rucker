d1f17219c59ea2a671e45f496e64eaf6462e6b10
LYErikuy <111875719+Buelie@users.noreply.github.com>
2023-01-14 15:54:07 +0800

    修复无法正常获取频道详情的问题

--- Rucker/Channel/Channel.js
+++ Rucker/Channel/Channel.js
@@ -4,49 +4,49 @@ xmlhttp.send()
 var ner = xmlhttp.responseText;
 
 function channel_id(){
-	return ner.data[0];
+	return ner.data.id;
 }
 
 function channel_guild_id(){
-	return ner.data[1];
+	return ner.data.guild_id;
 }
 
 function channel_user_id(){
-	return ner.data[2];
+	return ner.data.user_id;
 }
 
 function channel_parent_id(){
-	return ner.data[3];
+	return ner.data.parent_id;
 }
 
 function channel_name(){
-	return ner.data[4];
+	return ner.data.name;
 }
 
 function channel_topic(){
-	return ner.data[5];
+	return ner.data.topic;
 }
 
 function channel_type(){
-	return ner.data[6];
+	return ner.data.type;
 }
 
 function channel_level(){
-	return ner.data[7];
+	return ner.data.level;
 }
 
 function channel_slow_mode(){
-	return ner.data[8];
+	return ner.data.slow_mode;
 }
 
 function channel_limit_amount(){
-	return ner.data[9];
+	return ner.data.limit_amount;
 }
 
 function channel_is_category(){
-	return ner.data[10];
+	return ner.data.is_category;
 }
 
 function channel_server_url(){
-	return ner.data[11];
+	return ner.data.server_url;
 }
