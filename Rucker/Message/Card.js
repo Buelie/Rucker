1ce3282938e05c17aac609dec7ce7af7ccc7837b
LYErikuy <111875719+Buelie@users.noreply.github.com>
2023-01-15 09:55:58 +0800

    Feat: Send a card message

---
+++ Rucker/Message/Card.js
@@ -0,0 +1,13 @@
+import '../index.js';
+
+function send_kmarkdown(text){
+	var ner = {
+		"type" : 10,
+		"target_id" : id(),
+		"content" : text
+	}
+	
+	xmlhttp.open("POST","https://www.kookapp.cn/api/v3/message/create");
+	xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+	xmlhttp.send(ner);
+}
